a79a69fd83b2ba32afe741f2e19f571f6c6f7945
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-04-06 22:58:40 +0500

    Update jqcrud.js

--- jqcrud.js
+++ jqcrud.js
@@ -10,9 +10,25 @@ function addProduct() {
         method: "POST",
         data: {name, price},
         success: function(response) {
-            $('#products').append(`<div> <h4> Name : ${name} </h4> <h3> Price : ${price}</h3>`)
             //('#products').append('<div>RANA SOBAAN</div>')
+            readProduct();
         }
     });
 
+}
+
+function readProduct() {
+    $.ajax({
+        url: "https://sp21-labtask2-fa18-bcs-038.herokuapp.com/",
+        method: "GET",
+        success: function() {
+            var products = $('#products');
+
+            for(var i; i<response.length ; i++) {
+                var p = products[i];
+                $('#products').append(`<div> <h4> Name : ${p.name} </h4> <h3> Price : ${p.price}</h3>`)
+
+            }
+        }
+    })
 }
